6ae7f2850f373ac7194e4e081ecd6d20ee13f6f8
Anacarlags <anacarlags@ufpi.edu.br>
2026-08-11 20:57:08 -0300

    Enviar questoes 1 E 2

---
+++ antecesor-sucessor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    
+    static void Main()
+    {
+        Console.WriteLine("Digite Um Número:");
+        double numero = double.Parse(Console.ReadLine());
+
+        double antecessor = numero-1;
+        double sucesor = numero +1;
+
+         Console.WriteLine($"O Antecessor  é {antecessor}.");
+         Console.WriteLine($"O Seu Numero é  {numero}.");
+         Console.WriteLine($"O Antecessor  é {sucesor}.");
+    }
+}
---
+++ soma-dois/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita o primeiro número
+        Console.WriteLine("Digite o primeiro número:");
+        int numero1 = int.Parse(Console.ReadLine());
+        
+        // Solicita o segundo número
+        Console.WriteLine("Digite o segundo número:");
+        int numero2 = int.Parse(Console.ReadLine());
+        
+        // Calcula a soma
+        int soma = numero1 + numero2;
+        
+        // Exibe o resultado no formato esperado
+        Console.WriteLine($"A soma é {soma}.");
+    }
+}
