9d7f9da77c05df0442055adaefe64d13d3a8f59a
Anacarlags <anacarlags@ufpi.edu.br>
2026-08-11 21:14:37 -0300

    area do retangulo

---
+++ areaRetangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+    {
+        //Ler os Valores de Base e Altura do Retangulo
+         Console.WriteLine("Digite o Valor da Base:");
+         double valorBase = double.Parse(Console.ReadLine());
+
+         Console.WriteLine("Digite o Valor da Altura:");
+         double valorAltura = double.Parse(Console.ReadLine());
+
+         //Calculo de Area e Perimetro
+         double area = valorBase*valorAltura ;
+         double  perimetro = 2*(valorAltura+valorBase) ;
+
+        //Mostra os Resultados
+         Console.WriteLine($"O Valor da Are eh: {area}");
+         Console.WriteLine($"OValor do Perimetro eh : {perimetro}");
+
+        
+    }  
+}
