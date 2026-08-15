44f6da4d330e4a695551f36959f70119fb519615
Anacarlags <anacarlags@ufpi.edu.br>
2026-08-15 15:50:41 -0300

    Login criado

--- login/Program.cs
+++ login/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Login
+{
+    static void Main()
+    {// Dados corretos cadastrados no sistema
+        string usuarioCorreto = "admin";
+        string senhaCorreta = "12345678";
+
+        Console.WriteLine("=== TELA DE LOGIN ===");
+
+        // Pede o nome de usuário
+        Console.Write("Digite o usuário: ");
+        string usuarioDigitado = Console.ReadLine();
+
+        // Pede a senha
+        Console.Write("Digite a senha: ");
+        string senhaDigitada = Console.ReadLine();
+
+        // Compara os dados digitados com os corretos
+        if (usuarioDigitado == usuarioCorreto && senhaDigitada == senhaCorreta)
+        {
+            Console.WriteLine("\nLogin realizado com sucesso! Bem-vindo.");
+        }
+        else
+        {
+            Console.WriteLine("\nUsuário ou senha incorretos. Acesso negado.");
+        }
+
+        Console.WriteLine("\nPressione qualquer tecla para sair.");
+        Console.ReadKey();
+
+    }
+}
