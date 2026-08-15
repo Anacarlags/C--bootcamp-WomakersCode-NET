307ae5ef6a61decfe80578f9c7599e12d2c37f0e
Anacarlags <anacarlags@ufpi.edu.br>
2026-08-15 16:46:02 -0300

    feat:questao soma ate digitar zero

---
+++ Questões/somaAteZero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class SomaAteZero
+{
+    static void Main()
+    {
+        int soma = 0;
+        
+        while (true)
+        {
+            Console.Write("Digite um número Inteiro ou - 0 para sair): ");
+            int numero = int.Parse(Console.ReadLine());
+            
+            //caso digite zero encerra o programa
+            if (numero == 0) break;
+             soma += numero;
+        }
+    
+        Console.WriteLine($"Soma: {soma}");
+        
+    }
+}
